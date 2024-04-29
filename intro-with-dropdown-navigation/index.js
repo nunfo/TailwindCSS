e7eead864e31544f372da9c7796939a5ad4c6a98
jos <registro27@gmail.com>
2024-04-29 23:46:06 +0200

    Proyecto Intro Dropdown Navigation - 1280px y ajustes CSS

--- intro-with-dropdown-navigation/index.js
+++ intro-with-dropdown-navigation/index.js
@@ -1,9 +1,43 @@
-const hamburgerMenu =  document.querySelector('#hamburger-menu');
-const navMenu = document.querySelector('nav');
+const hamburgerMenu =  document.querySelector('.hamburger-menu');
+const navBack = document.querySelector('.back');
+const navFront = document.querySelector('.front');
 
+const btnFeatures = document.querySelector('.features-btn');
+const imgFeatures = document.querySelector('#features-img');
+const ulFeatures = document.querySelector('.features-ul');
 
+const btnCompany = document.querySelector('.company-btn');
+const imgCompany = document.querySelector('#company-img');
+const ulCompany = document.querySelector('.company-ul');
 
 hamburgerMenu.addEventListener('click', () => {  
   hamburgerMenu.classList.toggle('active');
-  navMenu.classList.toggle('active');
+  navBack.classList.toggle('active');
+  navFront.classList.toggle('active');
+});
+
+btnFeatures.addEventListener('click', () => {   
+  ulFeatures.classList.toggle('active');
+  if (ulFeatures.classList.contains('active')) {
+    imgFeatures.src = './images/icon-arrow-up.svg';
+  } else {
+    imgFeatures.src = './images/icon-arrow-down.svg';
+  };
+  if (ulCompany.classList.contains('active')) {
+    ulCompany.classList.remove('active');
+    imgCompany.src = './images/icon-arrow-down.svg';
+  };
+});
+
+btnCompany.addEventListener('click', () => {   
+  ulCompany.classList.toggle('active');
+  if (ulCompany.classList.contains('active')) {
+    imgCompany.src = './images/icon-arrow-up.svg';
+  } else {
+    imgCompany.src = './images/icon-arrow-down.svg';
+  };
+  if (ulFeatures.classList.contains('active')) {
+    ulFeatures.classList.remove('active');
+    imgFeatures.src = './images/icon-arrow-down.svg';
+  };
 });
